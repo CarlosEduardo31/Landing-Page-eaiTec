9f8a44dd986526cc3dee669bbe60568661ac1d53
CarlosEduardo31 <carlos.eduardo.3832@gmail.com>
2023-01-19 17:35:38 -0300

    Primeiro commit foi criado Menu, Home Footer

---
+++ main.js
@@ -0,0 +1,32 @@
+window.addEventListener('scroll', onScroll)
+const navigation = document.getElementById('navigation');
+onScroll()
+function onScroll() {
+  showNavOnScroll()
+
+}
+
+function showNavOnScroll() {
+  if (scrollY > 0) {
+    navigation.classList.add('scroll')
+  } else {
+    navigation.classList.remove('scroll')
+  }
+}
+
+function openMenu() {
+  document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+  document.body.classList.remove('menu-expanded')
+}
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700
+}).reveal(`
+  #home, 
+  #home img, 
+  #home .stats`)
